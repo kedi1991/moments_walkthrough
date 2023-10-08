104dd455912900303ad6f2446ecefb1a517cc963
kedi1991 <okurut.henry@yahoo.com>
2023-10-08 11:01:34 +0000

    Add css styling. each component should have its own css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import { Button } from 'react-bootstrap';
+import styles from './App.module.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Kedi</h1>
-      <Button variant="success">Success</Button>
+    <div className="{styles.App}">
+      <NavBar></NavBar>
     </div>
   );
 }
