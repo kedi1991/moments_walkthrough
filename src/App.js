08165ddd9807fe7b8c0d3a770027bcd6b03f19e5
kedi1991 <okurut.henry@yahoo.com>
2023-10-08 08:44:37 +0000

    Add react bootstrap and a button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Kedi</h1>
+      <Button variant="success">Success</Button>
     </div>
   );
 }
